a092da5b854327443e5ad7c6a67f38bb2d7758e8
Suraj Bhandarkar S <42907076+Suraj-Bhandarkar-S@users.noreply.github.com>
2020-11-15 10:56:10 +0530

    Calci updated

--- calculator/Calculator.py
+++ calculator/Calculator.py
@@ -5,8 +5,14 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 import threading
 from kivy.uix.image import Image
 from kivy.lang import Builder
-Config.set('graphics', 'width', '800')
-Config.set('graphics', 'height', '480')
+from kivy.core.window import Window
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+
+import math
+Config.set('graphics', 'width', '500') 
+Config.set('graphics', 'height', '400') 
 
 Builder.load_string("""
 <ScreenManagement>:
@@ -14,10 +20,22 @@ Builder.load_string("""
     MainScreen:
         id: main_screen
         name: 'osd_screen'
-    PauseScreen:
-        id: pause_screen
-        name: 'pause'
+    CalciScreen:
+        id: calci_screen
+        name: 'calci'
 
+
+<ScaleLabel@Label>:
+    _scale: 1. if self.texture_size[0] < self.width else float(self.width) / self.texture_size[0]
+    canvas.before:
+        PushMatrix
+        Scale:
+            origin: self.center
+            x: self._scale or 1.
+            y: self._scale or 1.
+    canvas.after:
+        PopMatrix
+        
 <MainScreen>:
     canvas.before:
         Color:
@@ -37,18 +55,77 @@ Builder.load_string("""
     BoxLayout:
         orientation: 'vertical'
         Label:
-            text: 'Welcome to Bajil Bois Association'
+           
             font_size: 25
 """)
-class PauseScreen(Screen):
+red = [1,0,0,1]
+green = [0,1,0,1]
+blue =  [0,0,1,1]
+purple = [1,0,1,1]
+import random
+class CalciScreen(Screen):
 
     def on_enter(self):
-        print('enter')
-        #Clock.schedule_once(self.switch_back, 5)
+        self.operators = ["/", "*", "+", "-"]
+        self.last_was_operator = None
+        self.last_button = None
+        main_layout = BoxLayout(orientation="vertical")
+        self.solution = TextInput(
+            multiline=False, readonly=True, halign="right", font_size=55
+        )
+        main_layout.add_widget(self.solution)
+        buttons = [
+            ["7", "8", "9", "/"],
+            ["4", "5", "6", "*"],
+            ["1", "2", "3", "-"],
+            [".", "0", "C", "+"],
+        ]
+        for row in buttons:
+            h_layout = BoxLayout()
+            for label in row:
+                button = Button(
+                    text=label,
+                    pos_hint={"center_x": 0.5, "center_y": 0.5},
+                )
+                button.bind(on_press=self.on_button_press)
+                h_layout.add_widget(button)
+            main_layout.add_widget(h_layout)
 
-    # def switch_back(self, *args):
-    #     print('back')
-    #     self.manager.current = "osd_screen"
+        equals_button = Button(
+            text="=", pos_hint={"center_x": 0.5, "center_y": 0.5}
+        )
+        equals_button.bind(on_press=self.on_solution)
+        main_layout.add_widget(equals_button)
+
+        self.add_widget(main_layout)
+
+    def on_button_press(self, instance):
+        current = self.solution.text
+        button_text = instance.text
+
+        if button_text == "C":
+            # Clear the solution widget
+            self.solution.text = ""
+        else:
+            if current and (
+                self.last_was_operator and button_text in self.operators):
+                # Don't add two operators right after each other
+                return
+            elif current == "" and button_text in self.operators:
+                # First character cannot be an operator
+                return
+            else:
+                new_text = current + button_text
+                self.solution.text = new_text
+        self.last_button = button_text
+        self.last_was_operator = self.last_button in self.operators
+
+    def on_solution(self, instance):
+        text = self.solution.text
+        if text:
+            solution = str(eval(self.solution.text))
+            self.solution.text = solution
+        
 
 
 class MainScreen(Screen):
@@ -57,14 +134,14 @@ class MainScreen(Screen):
         Clock.schedule_once(self.switch, 5)
 
     def switch(self, *args):
-        self.manager.current = "pause"
+        self.manager.current = "calci"
 
 
 class ScreenManagement(ScreenManager):
     stop = threading.Event()
 
 
-class OsdApp(App):
+class CalculatorApp(App):
     def on_stop(self):
         self.root.stop.set()
 
@@ -73,14 +150,4 @@ class OsdApp(App):
 
 
 if __name__ == '__main__':
-    OsdApp().run()
-    # class MainApp(App):
-    #     def build(self):
-    #         Window.clearcolor = (1, 1, 1, 1)
-    #         img = Image(source='./Images/Logo.png',size_hint=(1, .5),pos_hint={'center_x':.5, 'center_y':.5})
-    #         return img
-
-
-    # if __name__ == '__main__':
-    #     app = MainApp()
-    #     app.run()
+    CalculatorApp().run()
